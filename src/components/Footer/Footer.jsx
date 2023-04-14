5e936e68ada3a3f97250fedcf321c236fa2e01f9
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-04-14 11:50:29 +0300

    feat: add map

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.scss';
+import { Link } from 'react-router-dom';
+
+
+function Header (props) {
+
+  return (
+    <header className='footer'>
+        <div className="footer__wrapper">
+            <div className="header__content">
+
+            </div>
+        </div>    
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Map/MyMap.jsx
@@ -0,0 +1,48 @@
+import { MapContainer, TileLayer, Marker} from 'react-leaflet';
+import './MyMap.scss';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import marker from '../../assets/icon/map-marker.svg';
+import { useState } from 'react';
+import mapIcon from "../../assets/icon/map-address.svg"
+
+
+function MyMap() {
+  const [selectedPoint, setSelectedPoint] = useState("true");
+  const points = [
+    { id: 1, lat: 55.714369, lng: 37.673421, name: 'Дубровка', description: 'ст.м. Дубровка, 1-я улица Машиностроения, 16' },
+    { id: 2, lat: 55.611145, lng: 37.722386, name: 'Домодедовская', description: 'станция метро Домодедовская' },
+    { id: 3, lat: 55.608465, lng: 37.716612, name: 'Домодедовская', description: 'станция метро Домодедовская' },
+  ];
+  const customIcon = new Icon({
+    iconUrl: marker,
+    iconSize: [20, 20],
+  });
+
+  const handleMarkerClick = (point) => {
+    setSelectedPoint(point);
+  };
+
+  return (
+    <>
+      <div className="popup">
+        <h3 className='popup__title'>Контакты</h3>
+        <div className="popup__address-block">
+          <img src={mapIcon} alt="icon" />
+          <p className='popup__text'>Адрес</p>
+        </div>
+        <p className="popup__address">{selectedPoint.description}</p>
+      </div>
+      <MapContainer center={[55.714369, 37.673421]} zoom={11}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {points.map((point) => (
+         <Marker key={point.id} position={[point.lat, point.lng]} icon={customIcon} eventHandlers={{ click: () => handleMarkerClick(point) }} defaultVisible={true}/>
+        ))}
+
+      </MapContainer>
+    </>
+  );
+}
+
+
+export default MyMap;
