627706fd6f4eaa80d84e52502f19dc909cad0739
Buldakov <abuldakov10@gmail.com>
2023-05-04 14:16:25 +0400

    Fix btn style

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,28 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './ErrorPage.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import { Link } from 'react-router-dom';
 
-
-function ErrorPage({menu, icon}) {
-const [isPopupAccountOpen, setIsPopupAccountOpen] = useState(false);
+function ErrorPage({ menu, icon }) {
+  const [isPopupAccountOpen, setIsPopupAccountOpen] = useState(false);
 
   return (
-    <div className='error-page background'>
-      <Header menu={menu} icon={icon} isPopupAccountOpen={isPopupAccountOpen} setIsPopupAccountOpen={setIsPopupAccountOpen}/>
-      <section className='error-page__content wrapper'>
-        <h1 className='error-page__title'>Упс...</h1>
-        <h2 className='error-page__subtitle'>Такой страницы не существует </h2>
-        <p className='error-page__text'>Произошла ошибка, вернитесь на главную страницу</p>
-        <Link className='error-page__btn btn' to="/">На главную</Link>
+    <div className="error-page background">
+      <Header
+        menu={menu}
+        icon={icon}
+        isPopupAccountOpen={isPopupAccountOpen}
+        setIsPopupAccountOpen={setIsPopupAccountOpen}
+      />
+      <section className="error-page__content wrapper">
+        <h1 className="error-page__title">Упс...</h1>
+        <h2 className="error-page__subtitle">Такой страницы не существует </h2>
+        <p className="error-page__text">Произошла ошибка, вернитесь на главную страницу</p>
+        <Link className="error-page__btn btn btn--bg" to="/">
+          На главную
+        </Link>
       </section>
-      
-      <Footer menu={menu} icon={icon}/>
+
+      <Footer menu={menu} icon={icon} />
     </div>
-    )
-    
-  };
+  );
+}
 
 export default ErrorPage;
-
--- src/components/RegistrationPage/Confirmation/ConfirmationEmailPage.jsx
+++ src/components/RegistrationPage/Confirmation/ConfirmationEmailPage.jsx
@@ -1,42 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import {useNavigate } from 'react-router-dom'
-import "./Confirmation.scss"
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Confirmation.scss';
 import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
-import Api from "../../Api/Api";
+import Api from '../../Api/Api';
 
-function ConfirmationEmailPage (props) {
-  const { menu, icon} = props;
-  const navigate = useNavigate()
+function ConfirmationEmailPage(props) {
+  const { menu, icon } = props;
+  const navigate = useNavigate();
   const [isPopupAccountOpen, setIsPopupAccountOpen] = useState(false);
-  const [errors, setErrors] = useState("");
+  const [errors, setErrors] = useState('');
   useEffect(() => {
-    const accessToken = localStorage.getItem('access_token'); 
+    const accessToken = localStorage.getItem('access_token');
     if (accessToken) {
       navigate('/');
     }
   }, [navigate]);
-  
-  function submitForm(event) {
 
+  function submitForm(event) {
     event.preventDefault(); // предотвращаем стандартное поведение формы
     const form = event.target; // получаем форму из события
     const formData = new FormData(form); // создаем объект FormData
-  
 
     Api.post('api/v1/confirmation/', formData)
-      .then(response => {
-        localStorage.setItem("access_token", response.data.access);
-        localStorage.setItem("refresh_token", response.data.refresh);
+      .then((response) => {
+        localStorage.setItem('access_token', response.data.access);
+        localStorage.setItem('refresh_token', response.data.refresh);
         if (response.status === 201) {
-          navigate('/')
+          navigate('/');
         }
-
       })
       .catch((error) => {
         if (error.response.status === 400) {
           // Если есть ошибки валидации, отображаем их на странице
-         setErrors("Неверный код");
+          setErrors('Неверный код');
         }
       });
   }
@@ -44,22 +41,36 @@ function ConfirmationEmailPage (props) {
   const Form = () => {
     return (
       <div className="confirmation-page background">
-        <Header menu={menu} icon={icon} isPopupAccountOpen={isPopupAccountOpen} setIsPopupAccountOpen={setIsPopupAccountOpen}/>
-        <main className='confirmation-page__content wrapper'>
-          <h1 className='confirmation-page__title'>Регистрация</h1>
-          <h2 className='confirmation-page__subtitle'>Подтверждение почты</h2>
+        <Header
+          menu={menu}
+          icon={icon}
+          isPopupAccountOpen={isPopupAccountOpen}
+          setIsPopupAccountOpen={setIsPopupAccountOpen}
+        />
+        <main className="confirmation-page__content wrapper">
+          <h1 className="confirmation-page__title">Регистрация</h1>
+          <h2 className="confirmation-page__subtitle">Подтверждение почты</h2>
           <form className="confirmation-form" onSubmit={submitForm}>
-            <label  htmlFor="confirmation-email">На указанную почту было выслано письмо с кодом. Введите его в строке ниже</label>
-            <input type="text" id="confirmation-email" name="confirmation_token" placeholder='Проверочный код из письма '/>
-            <div className={`error ${errors? "error_active" : ""}`}>{errors}</div> 
-            <button type="submit" className='confirmation-form__btn btn'>Подтвердить</button>
+            <label htmlFor="confirmation-email">
+              На указанную почту было выслано письмо с кодом. Введите его в строке ниже
+            </label>
+            <input
+              type="text"
+              id="confirmation-email"
+              name="confirmation_token"
+              placeholder="Проверочный код из письма "
+            />
+            <div className={`error ${errors ? 'error_active' : ''}`}>{errors}</div>
+            <button type="submit" className="confirmation-form__btn btn btn--bg">
+              Подтвердить
+            </button>
           </form>
         </main>
-        <Footer menu={menu} icon={icon}/>
+        <Footer menu={menu} icon={icon} />
       </div>
-    )
-   }
+    );
+  };
 
-   return <Form />;
- }
+  return <Form />;
+}
 export default ConfirmationEmailPage;
