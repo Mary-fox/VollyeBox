05cec49c0532b0ccdde12b308bb3e2804054fa8e
Buldakov <abuldakov10@gmail.com>
2023-05-16 17:31:14 +0400

    Add default data and style to the level page. Move style variables to one file

--- src/pages/PlayerLevelPage/PlayerLevelPage.jsx
+++ src/pages/PlayerLevelPage/PlayerLevelPage.jsx
@@ -24,7 +24,8 @@ const PlayerLevelPage = () => {
   // Change level id
   useEffect(() => {
     if (activeLevelId) {
-      api.get(`player-level/${activeLevelId}/`).then(() => {
+      api.get(`player-level/${activeLevelId}/`).then(({ data }) => {
+        // console.log(data, 'data');
         // console.log(+activeLevelId, 'activeLevelId');
       });
     }
@@ -54,10 +55,80 @@ const PlayerLevelPage = () => {
       </section>
 
       {/*** Level info ***/}
-      <section>active level info</section>
+      <section className="level-info-section player-level">
+        <div className="player-level__preview">
+          <div className="player-level__preview-image">
+            <img
+              src="https://merlinsbeard.ru//media/filer_public/b2/53/b253323f-06c7-4eab-8a44-13431c220a5b/1660191934_2-sportishka-com-p-trenirovka-voleibol-devushki-sport-krasivo-2.jpg"
+              alt="image"
+            />
+          </div>
+
+          <div className="player-level__preview-grade grade">
+            <span className="grade__total">3</span>
+            <span className="grade__range">Оценка 3 из 9</span>
+          </div>
+        </div>
+
+        <nav className="player-level__nav">
+          <ul>
+            <li>Прием</li>
+            <li>Подача</li>
+            <li>Передача</li>
+            <li>Нападение</li>
+            <li>Блок</li>
+          </ul>
+        </nav>
+
+        <div className="player-level__details">
+          <p className="player-level__details-title">Подача</p>
+          <p className="player-level__details-subtitle">Частые ошибки</p>
+
+          <ul className="details-list">
+            <li className="details-list__item">
+              <p className="details-list__item-text">- неправильный разбег</p>
+            </li>
+            <li className="details-list__item">
+              <p className="details-list__item-text">- не попадание руками по мячу</p>
+            </li>
+            <li className="details-list__item">
+              <p className="details-list__item-text">- не попадание руками по мячу</p>
+            </li>
+            <li className="details-list__item">
+              <p className="details-list__item-text">- не попадание в площадку</p>
+            </li>
+            <li className="details-list__item">
+              <p className="details-list__item-text">- касание сетки</p>
+            </li>
+            <li className="details-list__item">
+              <p className="details-list__item-text">- неправильный выход под мяч</p>
+            </li>
+            <li className="details-list__item">
+              <p className="details-list__item-text">- низкий прыжок</p>
+            </li>
+            <li className="details-list__item">
+              <p className="details-list__item-text">- прыжок вперед</p>
+            </li>
+          </ul>
+        </div>
+      </section>
 
       {/*** Video ***/}
-      <section>video</section>
+      <section className="level-video-section level-video">
+        <div className="level-video__wrapper">
+          <iframe
+            // width="560"
+            // height="315"
+            src="https://www.youtube.com/embed/7qnrPk0cFs8"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          />
+        </div>
+
+        <span className="level-video__title">Подача</span>
+      </section>
     </div>
   );
 };
