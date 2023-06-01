4a66796af523c96108e98ca5ccbaa204bfb0a2e2
Buldakov <abuldakov10@gmail.com>
2023-06-01 20:35:27 +0400

    Add main desktop filter

--- src/pages/SchedulePage/ScheduleFilter/ScheduleFilter.jsx
+++ src/pages/SchedulePage/ScheduleFilter/ScheduleFilter.jsx
@@ -1,8 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Menu, MenuItem } from '@mui/material';
 
 // Files
 import './ScheduleFilter.scss';
+import { api } from '../../../constants/constants';
 
 // Components
 import DropDownIcon from '../../../components/IconComponents/DropDownIcon';
@@ -14,13 +16,103 @@ const ScheduleFilter = () => {
   // Get data for menu filter from context
   const { trainers, gym, level, trainingType } = useContext(MenuFilterContext);
 
-  // Filter menu states
-  const [trainersMenu, setTrainersMenu] = useState(null); // Trainer menu
-  const [gymMenu, setGymMenu] = useState(null); // Gym menu
-  const [levelMenu, setLevelMenu] = useState(null); // Level menu
-  const [trainingTypeMenu, setTrainingTypeMenu] = useState(null); // Training menu
-  const [genderMenu, setGenderMenu] = useState(null); // Gender menu
+  // Pseudo api data for gender
+  const gender = [
+    { id: 1, title: 'a', name: 'Общий' },
+    { id: 2, title: 'm', name: 'Мужской' },
+    { id: 3, title: 'f', name: 'Женский' },
+  ];
 
+  /*** Filter menu states ***/
+  // Trainer menu and active trainer filter state
+  const [trainersMenu, setTrainersMenu] = useState(null);
+  const [activeTrainerFilter, setActiveTrainerFilter] = useState('тренер');
+
+  // Gym menu and active gym filter state
+  const [gymMenu, setGymMenu] = useState(null);
+  const [activeGymFilter, setActiveGymFilter] = useState('зал');
+
+  // Level menu and active level filter state
+  const [levelMenu, setLevelMenu] = useState(null);
+  const [activeLevelFilter, setActiveLevelFilter] = useState('уровень');
+
+  // Training menu and active training type filter state
+  const [trainingTypeMenu, setTrainingTypeMenu] = useState(null);
+  const [activeTrainingTypeFilter, setActiveTrainingTypeFilter] = useState('тип тренировки');
+
+  // Gender menu and active gender filter state
+  const [genderMenu, setGenderMenu] = useState(null);
+  const [activeGenderFilter, setActiveGenderFilter] = useState('гендер тренировки');
+
+  /*** Filter search params ***/
+  const [filterParams, setFilterParams] = useSearchParams();
+
+  // Get klass data with/without params on page load
+  useEffect(() => {
+    let requestParams = '';
+
+    // Check if search params - make new str for request with params
+    if (filterParams.toString() !== '') {
+      // Make new string for request with params
+      const requestStr = [];
+
+      for (let [key, value] of filterParams.entries()) {
+        requestStr.push(`${key}=${value}`);
+      }
+
+      requestParams = `?${requestStr.join('&')}`;
+    }
+
+    // Получение всех занятий
+    api.get(`klass/${requestParams}`).then(({ data }) => {
+      console.log(data, 'klass data on load page');
+    });
+  }, []);
+
+  // Set selected params to filter menu on page load
+  useEffect(() => {
+    // Set trainer name if it's in params
+    if (filterParams.has('trainer') && trainers.length > 0) {
+      const paramValue = filterParams.get('trainer');
+      const activeTrainerObject = trainers.filter(({ id }) => +id === +paramValue)[0];
+
+      setActiveTrainerFilter(`${activeTrainerObject.last_name} ${activeTrainerObject.first_name}`);
+    }
+
+    // Set gym name if it's in params
+    if (filterParams.has('gym') && gym.length > 0) {
+      const paramValue = filterParams.get('gym');
+      const activeGymObject = gym.filter(({ id }) => +id === +paramValue)[0];
+
+      setActiveGymFilter(activeGymObject.name);
+    }
+
+    // Set level name if it's in params
+    if (filterParams.has('level') && level.length > 0) {
+      const paramValue = filterParams.get('level');
+      const activeLevelObject = level.filter(({ id }) => +id === +paramValue)[0];
+
+      setActiveLevelFilter(activeLevelObject.title);
+    }
+
+    // Set training-type name if it's in params
+    if (filterParams.has('type') && trainingType.length > 0) {
+      const paramValue = filterParams.get('type');
+      const activeTrainingTypeObject = trainingType.filter(({ id }) => +id === +paramValue)[0];
+
+      setActiveTrainingTypeFilter(activeTrainingTypeObject.title);
+    }
+
+    // Set gender name if it's in params
+    if (filterParams.has('gender')) {
+      const paramValue = filterParams.get('gender');
+      const activeGenderObject = gender.filter(({ title }) => title === paramValue)[0];
+
+      setActiveGenderFilter(activeGenderObject.name);
+    }
+  }, [trainers, gym, level, trainingType]);
+
+  /*** Handlers ***/
   // Close all previous opened menu
   const handleClosePreviousMenu = () => {
     setTrainersMenu(null);
@@ -30,8 +122,74 @@ const ScheduleFilter = () => {
     setGenderMenu(null);
   };
 
+  // Получение отфильтрованных занятий
+  const addFilterHandler = (filterType, id, genderTitle) => {
+    // Если выбор гендера, то использовать его title вместо id
+    if (genderTitle) {
+      id = genderTitle;
+    }
+
+    // Если параметр этого фильтра с таким значением уже есть, то не делать никаких запросов
+    if (filterParams.has(filterType) && filterParams.get(filterType) === id.toString()) {
+      handleClosePreviousMenu();
+      return;
+    }
+
+    // Создать новый объект с параметрами
+    const newParamsObj = {};
+
+    // Взять существующие параметры и сформировать новый объект
+    for (let [key, value] of filterParams.entries()) {
+      newParamsObj[key] = value;
+    }
+
+    // Добавить новый параметр в объекты фильтра
+    newParamsObj[filterType] = id;
+
+    // Сформировать строку для запроса
+    const paramsString = Object.entries(newParamsObj)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&');
+
+    // Установить параметры в адресную строку
+    setFilterParams(newParamsObj);
+
+    // Запрос для фильтрации
+    api.get(`klass/?${paramsString}`).then(({ data }) => {
+      console.log(data, 'klass data after add filter param');
+    });
+  };
+
+  // Получение отфильтрованных занятий после удаления параметра (пункт ---Все---)
+  const deleteFilterHandler = (deleteParam) => {
+    // Удаление параметра
+    filterParams.delete(deleteParam);
+
+    // Создать новый объект с параметрами
+    const newParamsObj = {};
+
+    // Взять существующие параметры и сформировать новый объект
+    for (let [key, value] of filterParams.entries()) {
+      newParamsObj[key] = value;
+    }
+
+    // Сформировать строку для запроса
+    const paramsString = Object.entries(newParamsObj)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&');
+
+    // Установить параметры в адресную строку
+    setFilterParams(newParamsObj);
+
+    // Запрос для фильтрации
+    api.get(`klass/?${paramsString}`).then(({ data }) => {
+      console.log(data, 'klass data after delete filter param');
+    });
+  };
+
   return (
     <ul className="schedule-main-filter">
+      {/* Trainer */}
       <li className="schedule-main-filter__item">
         <span
           className={`schedule-main-filter__title ${trainersMenu ? 'expanded' : ''}`}
@@ -42,7 +200,7 @@ const ScheduleFilter = () => {
             setTrainersMenu(event.currentTarget);
           }}
         >
-          тренер
+          {activeTrainerFilter}
           <DropDownIcon />
         </span>
 
@@ -53,11 +211,32 @@ const ScheduleFilter = () => {
           open={!!trainersMenu}
           onClose={handleClosePreviousMenu}
         >
-          <MenuItem onClick={handleClosePreviousMenu}>--- Все ---</MenuItem>
+          <MenuItem
+            onClick={() => {
+              // Если параметра этого фильтра нет, то не делать никаких запросов
+              if (!filterParams.has('trainer')) {
+                handleClosePreviousMenu();
+                return;
+              }
+
+              setActiveTrainerFilter('тренер'); // Set default value
+              deleteFilterHandler('trainer'); // Delete param
+              handleClosePreviousMenu();
+            }}
+          >
+            --- Все ---
+          </MenuItem>
 
           {trainers.map(({ id, first_name, last_name }) => {
             return (
-              <MenuItem onClick={handleClosePreviousMenu} key={id}>
+              <MenuItem
+                key={id}
+                onClick={() => {
+                  addFilterHandler('trainer', id); // Add filter param
+                  setActiveTrainerFilter(`${last_name} ${first_name}`); // Set selected name
+                  handleClosePreviousMenu();
+                }}
+              >
                 {last_name} {first_name}
               </MenuItem>
             );
@@ -65,6 +244,7 @@ const ScheduleFilter = () => {
         </Menu>
       </li>
 
+      {/* Gym */}
       <li className="schedule-main-filter__item">
         <span
           className={`schedule-main-filter__title ${gymMenu ? 'expanded' : ''}`}
@@ -75,7 +255,7 @@ const ScheduleFilter = () => {
             setGymMenu(event.currentTarget);
           }}
         >
-          Зал
+          {activeGymFilter}
           <DropDownIcon />
         </span>
 
@@ -86,12 +266,33 @@ const ScheduleFilter = () => {
           open={!!gymMenu}
           onClose={handleClosePreviousMenu}
         >
-          <MenuItem onClick={handleClosePreviousMenu}>--- Все ---</MenuItem>
+          <MenuItem
+            onClick={() => {
+              // Если параметра этого фильтра нет, то не делать никаких запросов
+              if (!filterParams.has('gym')) {
+                handleClosePreviousMenu();
+                return;
+              }
+
+              setActiveGymFilter('зал'); // Set default value
+              deleteFilterHandler('gym'); // Delete param
+              handleClosePreviousMenu();
+            }}
+          >
+            --- Все ---
+          </MenuItem>
 
           {gym.map(({ id, name, is_published }) => {
             if (is_published) {
               return (
-                <MenuItem onClick={handleClosePreviousMenu} key={id}>
+                <MenuItem
+                  key={id}
+                  onClick={() => {
+                    addFilterHandler('gym', id); // Add filter param
+                    setActiveGymFilter(name); // Set selected name
+                    handleClosePreviousMenu();
+                  }}
+                >
                   {name}
                 </MenuItem>
               );
@@ -100,6 +301,7 @@ const ScheduleFilter = () => {
         </Menu>
       </li>
 
+      {/* Level */}
       <li className="schedule-main-filter__item">
         <span
           className={`schedule-main-filter__title ${levelMenu ? 'expanded' : ''}`}
@@ -110,7 +312,7 @@ const ScheduleFilter = () => {
             setLevelMenu(event.currentTarget);
           }}
         >
-          уровень
+          {activeLevelFilter}
           <DropDownIcon />
         </span>
 
@@ -121,12 +323,33 @@ const ScheduleFilter = () => {
           open={!!levelMenu}
           onClose={handleClosePreviousMenu}
         >
-          <MenuItem onClick={handleClosePreviousMenu}>--- Все ---</MenuItem>
+          <MenuItem
+            onClick={() => {
+              // Если параметра этого фильтра нет, то не делать никаких запросов
+              if (!filterParams.has('level')) {
+                handleClosePreviousMenu();
+                return;
+              }
+
+              setActiveLevelFilter('уровень'); // Set default value
+              deleteFilterHandler('level'); // Delete param
+              handleClosePreviousMenu();
+            }}
+          >
+            --- Все ---
+          </MenuItem>
 
           {level.map(({ id, title, is_published }) => {
             if (is_published) {
               return (
-                <MenuItem onClick={handleClosePreviousMenu} key={id}>
+                <MenuItem
+                  key={id}
+                  onClick={() => {
+                    addFilterHandler('level', id); // Add filter param
+                    setActiveLevelFilter(title); // Set selected name
+                    handleClosePreviousMenu();
+                  }}
+                >
                   {title}
                 </MenuItem>
               );
@@ -135,6 +358,7 @@ const ScheduleFilter = () => {
         </Menu>
       </li>
 
+      {/* Training type */}
       <li className="schedule-main-filter__item">
         <span
           className={`schedule-main-filter__title ${trainingTypeMenu ? 'expanded' : ''}`}
@@ -145,7 +369,7 @@ const ScheduleFilter = () => {
             setTrainingTypeMenu(event.currentTarget);
           }}
         >
-          Тип тренировки
+          {activeTrainingTypeFilter}
           <DropDownIcon />
         </span>
 
@@ -156,12 +380,33 @@ const ScheduleFilter = () => {
           open={!!trainingTypeMenu}
           onClose={handleClosePreviousMenu}
         >
-          <MenuItem onClick={handleClosePreviousMenu}>--- Все ---</MenuItem>
+          <MenuItem
+            onClick={() => {
+              // Если параметра этого фильтра нет, то не делать никаких запросов
+              if (!filterParams.has('type')) {
+                handleClosePreviousMenu();
+                return;
+              }
+
+              setActiveTrainingTypeFilter('тип тренировки'); // Set default value
+              deleteFilterHandler('type'); // Delete param
+              handleClosePreviousMenu();
+            }}
+          >
+            --- Все ---
+          </MenuItem>
 
           {trainingType.map(({ id, title, is_published }) => {
             if (is_published) {
               return (
-                <MenuItem onClick={handleClosePreviousMenu} key={id}>
+                <MenuItem
+                  key={id}
+                  onClick={() => {
+                    addFilterHandler('type', id); // Add filter param
+                    setActiveTrainingTypeFilter(title); // Set selected name
+                    handleClosePreviousMenu();
+                  }}
+                >
                   {title}
                 </MenuItem>
               );
@@ -170,6 +415,7 @@ const ScheduleFilter = () => {
         </Menu>
       </li>
 
+      {/* Gender */}
       <li className="schedule-main-filter__item">
         <span
           className={`schedule-main-filter__title ${genderMenu ? 'expanded' : ''}`}
@@ -180,7 +426,7 @@ const ScheduleFilter = () => {
             setGenderMenu(event.currentTarget);
           }}
         >
-          Гендер тренеровки
+          {activeGenderFilter}
           <DropDownIcon />
         </span>
 
@@ -191,9 +437,36 @@ const ScheduleFilter = () => {
           open={!!genderMenu}
           onClose={handleClosePreviousMenu}
         >
-          <MenuItem onClick={handleClosePreviousMenu}>--- Все ---</MenuItem>
-          <MenuItem onClick={handleClosePreviousMenu}>Мужчина</MenuItem>
-          <MenuItem onClick={handleClosePreviousMenu}>Женцина</MenuItem>
+          <MenuItem
+            onClick={() => {
+              // Если параметра этого фильтра нет, то не делать никаких запросов
+              if (!filterParams.has('gender')) {
+                handleClosePreviousMenu();
+                return;
+              }
+
+              setActiveGenderFilter('Гендер тренеровки'); // Set default value
+              deleteFilterHandler('gender'); // Delete param
+              handleClosePreviousMenu();
+            }}
+          >
+            --- Все ---
+          </MenuItem>
+
+          {gender.map(({ id, title, name }) => {
+            return (
+              <MenuItem
+                key={id}
+                onClick={() => {
+                  addFilterHandler('gender', id, title); // Add filter param
+                  setActiveGenderFilter(name); // Set selected name
+                  handleClosePreviousMenu();
+                }}
+              >
+                {name}
+              </MenuItem>
+            );
+          })}
         </Menu>
       </li>
     </ul>
